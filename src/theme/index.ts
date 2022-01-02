ee8d4352de1679070b75f9721f9ef5f18a9dc05b
Diego <dglpferreira@gmail.com>
2022-01-02 11:09:40 -0300

    refactor: Added colors file
      Moved gruvbbox color scheme to the file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,32 +1,5 @@
 import { createTheme } from "@shopify/restyle";
-
-// gruvbox_material_medium_dark
-const gruvbox = { 
-  primary: {
-    background: "#282828",
-    foreground: "#d4be98",
-  },
-  normal: {
-    black: "#3c3836",
-    red: "#ea6962",
-    green: "#a9b665",
-    yellow: "#d8a657",
-    blue: "#7daea3",
-    magenta: "#d3869b",
-    cyan: "#89b482",
-    white: "#d4be98",
-  },
-  bright: {
-    black: "#3c3836",
-    red: "#ea6962",
-    green: "#a9b665",
-    yellow: "#d8a657",
-    blue: "#7daea3",
-    magenta: "#d3869b",
-    cyan: "#89b482",
-    white: "#d4be98",
-  },
-};
+import gruvbox from './colors';
 
 const appDarkTheme = createTheme({
   colors: {
